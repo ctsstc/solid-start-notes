23e1dc0a8b27e1ef54156cbd2f9df91184448d59
Cody Swartz <ctsstc@gmail.com>
2024-03-25 00:37:40 -0700

    Extract sidebar inline styles into app css
    This will also allow us to properly dark theme it.

--- src/components/SidebarNote.tsx
+++ src/components/SidebarNote.tsx
@@ -6,9 +6,10 @@
  *
  */
 
-import { useLocation } from "@solidjs/router";
-import { createEffect, createSignal, Show, useTransition } from "solid-js";
-import { Note } from "~/lib/types";
+import { createEffect, createSignal, Show, useTransition } from 'solid-js'
+import { Note } from '~/lib/types'
+import { classHelper } from '~/utilities/class-helper'
+import { useLocation } from '@solidjs/router'
 
 export default function SidebarNote(props: { note: Note }) {
   const location = useLocation();
@@ -34,9 +35,12 @@ export default function SidebarNote(props: { note: Note }) {
         itemRef.classList.remove("flash");
       }}
       style={{
-        color: "black"
+        color: "black",
       }}
-      class={["sidebar-note-list-item", isExpanded() ? "note-expanded" : ""].join(" ")}
+      class={classHelper(
+        "sidebar-note-list-item",
+        isExpanded() ? "note-expanded" : ""
+      )}
     >
       <header class="sidebar-note-header">
         <strong>{props.note.title}</strong>
@@ -44,38 +48,44 @@ export default function SidebarNote(props: { note: Note }) {
       </header>
       <a
         href={`/notes/${props.note.id}`}
-        class="sidebar-note-open"
-        style={{
-          "background-color": isPending()
-            ? "var(--gray-80)"
-            : isActive()
-            ? "var(--tertiary-blue)"
-            : "",
-          border: isActive() ? "1px solid var(--primary-border)" : "1px solid transparent"
-        }}
+        class={classHelper(
+          "sidebar-note-open",
+          isPending() ? "is-pending" : "",
+          isActive() ? "is-active" : ""
+        )}
       >
         Open note for preview
       </a>
       <button
         class="sidebar-note-toggle-expand"
-        onClick={e => {
+        onClick={(e) => {
           e.stopPropagation();
-          setIsExpanded(isExpanded => !isExpanded);
+          setIsExpanded((isExpanded) => !isExpanded);
         }}
       >
         <Show
           when={isExpanded()}
-          fallback={<img src="/chevron-down.svg" width="10px" height="10px" alt="Collapse" />}
+          fallback={
+            <img
+              src="/chevron-down.svg"
+              width="10px"
+              height="10px"
+              alt="Collapse"
+            />
+          }
         >
           <img src="/chevron-up.svg" width="10px" height="10px" alt="Expand" />
         </Show>
       </button>
       <div
         style={{
-          display: isExpanded() ? "block" : "none"
+          display: isExpanded() ? "block" : "none",
         }}
       >
-        <p class="sidebar-note-excerpt" innerHTML={props.note.body || `<i>No content</i>`} />
+        <p
+          class="sidebar-note-excerpt"
+          innerHTML={props.note.body || `<i>No content</i>`}
+        />
       </div>
     </div>
   );
